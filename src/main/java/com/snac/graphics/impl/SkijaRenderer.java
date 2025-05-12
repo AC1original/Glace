e68ab44a5be57cc69a19ee9ac86546768d3438e6
AC1original <ac1original.lol@gmail.com>
2025-05-12 22:31:23 +0200

    :sparkles: Using Skija with LWJGL for rendering

---
+++ src/main/java/com/snac/graphics/impl/SkijaRenderer.java
@@ -0,0 +1,218 @@
+package com.snac.graphics.impl;
+
+import com.snac.graphics.Brush;
+import com.snac.graphics.Canvas;
+import com.snac.graphics.Renderer;
+import com.snac.util.Loop;
+import de.snac.Ez2Log;
+import io.github.humbleui.skija.*;
+import io.github.humbleui.skija.impl.Stats;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+@Getter
+public class SkijaRenderer implements Renderer {
+    private long window = 0;
+    private volatile Canvas canvas;
+    private volatile boolean vsync = false;
+    private volatile int maxFps = 60;
+    private volatile int fps = 0;
+    private int width;
+    private int height;
+    private float dpi = 1f;
+    private Brush<Integer> brush;
+    private DirectContext context;
+    private BackendRenderTarget renderTarget;
+    private Surface surface;
+    private io.github.humbleui.skija.Canvas skijaCanvas;
+
+    public SkijaRenderer() {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        Ez2Log.info(this, "Initialized");
+    }
+
+    private void updateDimensions() {
+        int[] width = new int[1];
+        int[] height = new int[1];
+        glfwGetFramebufferSize(window, width, height);
+
+        float[] xscale = new float[1];
+        float[] yscale = new float[1];
+        glfwGetWindowContentScale(window, xscale, yscale);
+        assert xscale[0] == yscale[0] : "Horizontal dpi=" + xscale[0] + ", vertical dpi=" + yscale[0];
+
+        this.width = (int) (width[0] / xscale[0]);
+        this.height = (int) (height[0] / yscale[0]);
+        this.dpi = xscale[0];
+    }
+
+    @Override
+    public void createWindow(int width, int height, @NotNull String title) {
+        if (window != 0) {
+            Ez2Log.warn(this, "Could not create window, only one window per renderer is allowed.");
+            return;
+        }
+
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        this.window = glfwCreateWindow(width, height, title, 0, 0);
+        if (window == 0) {
+            throw new IllegalStateException("Unable to create window");
+        }
+
+        updateDimensions();
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(vsync ? 1 : 0);
+        glfwShowWindow(window);
+
+        Ez2Log.info(this, "Created window. Starting render loop");
+
+        startRenderLoop();
+    }
+
+    private synchronized void startRenderLoop() {
+        GL.createCapabilities();
+        context = DirectContext.makeGL();
+        glfwSetWindowSizeCallback(window, (window, width, height) -> {
+            updateDimensions();
+            initSkija();
+            render();
+        });
+        initSkija();
+
+
+        var loop = new Loop()
+                .runOnThread(true)
+                .setThreadName("Glace-Rendering");
+
+        loop.start(maxFps, fps -> {
+            this.fps = fps;
+
+            render();
+            glfwPollEvents();
+
+            if (glfwWindowShouldClose(window)) {
+                loop.stop();
+                Ez2Log.info(this, "LWJGL has been terminated. Render loop stopped");
+            }
+        }, () -> {
+            glfwFreeCallbacks(window);
+            glfwDestroyWindow(window);
+            glfwTerminate();
+            GLFWErrorCallback errorCallback = glfwSetErrorCallback(null);
+            if (errorCallback != null) errorCallback.free();
+
+            if (context != null) {
+                context.close();
+            }
+
+            Ez2Log.info(this, "Shutting down render loop");
+        });
+    }
+
+    private void initSkija() {
+        Stats.enabled = true;
+
+        if (surface != null) {
+            surface.close();
+        }
+        if (renderTarget != null) {
+            renderTarget.close();
+        }
+
+        renderTarget = BackendRenderTarget.makeGL(
+                (int) (width * dpi),
+                (int) (height * dpi),
+                /*samples*/0,
+                /*stencil*/8,
+                /*fbId*/0,
+                FramebufferFormat.GR_GL_RGBA8);
+
+        surface = Surface.makeFromBackendRenderTarget(
+                context,
+                renderTarget,
+                SurfaceOrigin.BOTTOM_LEFT,
+                SurfaceColorFormat.RGBA_8888,
+                ColorSpace.getDisplayP3(),
+                new SurfaceProps(PixelGeometry.RGB_H));
+
+        skijaCanvas = surface.getCanvas();
+        brush = new SkijaBrush(skijaCanvas, window);
+    }
+
+    @Override
+    public void moveWindow(int x, int y) {
+        glfwSetWindowPos(window, x, y);
+    }
+
+    @Override
+    public void resizeWindow(int width, int height) {
+        glfwSetWindowSize(window, width, height);
+    }
+
+    @Override
+    public void destroyWindow() {
+        if (window == 0) {
+            Ez2Log.warn(this, "Could not destroy window, window is not initialized");
+            return;
+        }
+        glfwSetWindowShouldClose(window, true);
+        window = 0;
+    }
+
+
+    @Override
+    public void render() {
+        if (getCanvas() == null) return;
+        glfwSwapBuffers(window);
+        getCanvas().render(brush);
+    }
+
+    @Override
+    public void setVSync(boolean vsync) {
+        this.vsync = vsync;
+        glfwSwapInterval(vsync ? 1 : 0);
+    }
+
+    @Override
+    public boolean isVSync() {
+        return vsync;
+    }
+
+    @Override
+    public void setCanvas(Canvas canvas) {
+        this.canvas = canvas;
+    }
+
+    @Override
+    @Nullable
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    @Override
+    public int getMaxFPS() {
+        return maxFps;
+    }
+
+    @Override
+    public void setMaxFPS(int fps) {
+        this.maxFps = fps;
+    }
+
+    @Override
+    public int getFPS() {
+        return fps;
+    }
+}
