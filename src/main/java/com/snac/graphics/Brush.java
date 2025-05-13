c39356ff356fede723e1d36a4b0daaa2cbb0e8e4
AC1original <ac1original.lol@gmail.com>
2025-05-14 00:19:26 +0200

    :sparkles: Finished Brush interface I think

--- src/main/java/com/snac/graphics/Brush.java
+++ src/main/java/com/snac/graphics/Brush.java
@@ -1,8 +1,32 @@
 package com.snac.graphics;
 
-public interface Brush<I> {
+import java.awt.*;
 
-    void drawRectangle(int x, int y, int width, int height);
+public interface Brush<I, F> {
+
+    void setFont(F font);
+
+    void setColor(Color color);
+
+    void drawRectangle(int x, int y, int width, int height, boolean filled);
 
     void drawImage(I image, int x, int y, int width, int height);
+
+    void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle, boolean filled);
+
+    void drawLine(int x1, int y1, int x2, int y2);
+
+    void drawPolygon(int[] xPoints, int[] yPoints, int nPoints, boolean filled);
+
+    void drawOval(int x, int y, int width, int height, boolean filled);
+
+    void drawRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight, boolean filled);
+
+    void drawText(String text, int x, int y);
+
+    void drawPixel(int x, int y);
+    void drawPixel(Point location);
+
+    void drawPixels(Point[] locations);
+    void drawPixels(Point[] locations, int[] colors);
 }
