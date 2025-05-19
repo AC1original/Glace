488b4c59bd793b0ab4fbff2f70582a5cabc38646
AC1original <ac1original.lol@gmail.com>
2025-05-19 23:48:11 +0200

    :bulb: Add documentation to Loop.java

--- src/main/java/com/snac/util/Loop.java
+++ src/main/java/com/snac/util/Loop.java
@@ -9,6 +9,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
+/**
+ * This class is used to create (Game-)Loops with a specific tps frequency
+ */
 @Getter
 public class Loop {
     private boolean running = false;
@@ -17,22 +20,56 @@ public class Loop {
     private final String threadName;
     private ExecutorService executorService;
 
+    /**
+     * There are two ways to create a new Loop instance:
+     * <br>1. Use this contractor
+     * <br>2. Use the builder generated from lombok (thank you lombok <3) For example:
+     * <pre>{@code
+     * var loop = Loop.builder().runOnThread(true).build();
+     * }</pre>
+     *
+     * @param runOnThread If set to {@code true} this Loop will use its own thread.
+     *                    Otherwise, the thread started on (Not recommended as it will block the entire thread)
+     * @param threadName Sets the name for the generated thread. This only makes sense if runOnThread-parameter is set to {@code true}
+     */
     @Builder
-    public Loop(boolean runOnThread, @Nullable String threadName, @Nullable ExecutorService specificExecutor) {
+    public Loop(boolean runOnThread, @Nullable String threadName) {
         this.runOnThread = runOnThread;
         this.threadName = threadName == null ? "" : threadName;
-        this.executorService = specificExecutor;
     }
 
-    public Loop start(final int TARGET_TPS) {
-        return start(() -> {}, TARGET_TPS, (tps) -> {}, () -> {});
+    /**
+     * Shorter version of {@link #start(Runnable, int, Consumer, Runnable)}
+     * @param TARGET_TPS The maximum tps this loop should tick. If you're confused or something just type 60 but NEVER 0 or lower
+     * @param action This consumer gets called every tick with the current fps
+     */
+    public void start(final int TARGET_TPS, Consumer<Integer> action) {
+        start(() -> {}, TARGET_TPS, action, () -> {});
     }
 
-    public Loop start(Runnable preRun, final int TARGET_TPS, Consumer<Integer> action, Runnable shutdownHook) {
+    /**
+     * Everything ready? To start the loop, you'll need to call this method.
+     * <br>Example how to use:
+     * <pre>{@code
+     * var loop = Loop.builder().runOnThread(true).build();
+     *
+     * loop.start(() -> System.out.println("This gets called before loop"),
+     *          60,
+     *          (currentFPS) -> render(),
+     *          () -> System.out.println("This gets called after loop"));
+     * }</pre>
+     * @param preRun This runnable gets called before the loop starts.
+     *               Used to run something on the same thread as this loop.
+     *               IDK if this is useful.
+     * @param TARGET_TPS The maximum tps this loop should tick. If you're confused or something just type 60 but NEVER 0 or lower
+     * @param action This consumer gets called every tick with the current fps
+     * @param shutdownHook This runnable gets called when this loop stops.
+     */
+    public void start(final Runnable preRun, final int TARGET_TPS, final Consumer<Integer> action, final Runnable shutdownHook) {
         if (!running) {
             running = true;
         } else {
-            return this;
+            return;
         }
 
         if (executorService == null) {
@@ -102,9 +139,13 @@ public class Loop {
             preRun.run();
             runnable.run();
         }
-        return this;
     }
 
+    /**
+     * You can stop the loop at any time with this methode.
+     * To start the loop again,
+     * you have to call {@link #start(Runnable, int, Consumer, Runnable)} or {@link #start(int, Consumer)}
+     */
     public void stop() {
         running = false;
         if (executorService != null) {
@@ -112,10 +153,16 @@ public class Loop {
         }
     }
 
+    /**
+     * Pauses the loop.
+     */
     public void pause() {
         paused = true;
     }
 
+    /**
+     * Unpauses the loop. This can take up to 20 ms.
+     */
     public void resume() {
         paused = false;
     }
