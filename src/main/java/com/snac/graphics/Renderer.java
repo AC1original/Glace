79a6637abd5c6bc0dc315f09b2bff3e140f0a963
AC1original <ac1original.lol@gmail.com>
2025-05-20 00:14:12 +0200

    :bulb: Add documentation to Renderer.java

--- src/main/java/com/snac/graphics/Renderer.java
+++ src/main/java/com/snac/graphics/Renderer.java
@@ -1,28 +1,82 @@
 package com.snac.graphics;
 
+/**
+ * This interface provides the structure for a Renderer. <br>
+ * A Renderer is meant to only handle one window.
+ * If you want to have more windows, you need to have more Renderer(-Instances)
+ * <p>
+ * Use this interface if you want to create your own Glace-renderer.
+ */
 public interface Renderer {
 
+    /**
+     * Creates a new window.
+     * @param width The window width
+     * @param height The window height
+     * @param title The windows title
+     */
     void createWindow(int width, int height, String title);
 
+    /**
+     * Move the window to a specific position
+     * @param x The new window X-Position
+     * @param y The new window Y-Position
+     */
     void moveWindow(int x, int y);
 
+    /**
+     * Resize the window.
+     * @param width The new window width
+     * @param height The new window height
+     */
     void resizeWindow(int width, int height);
 
+    /**
+     * Destroy the current window
+     */
     void destroyWindow();
 
+    /**
+     * Activate VSync. Also see {@link #isVSync()}
+     * @param vsync Set to {@code true} VSync is activated, otherwise disabled
+     */
     void setVSync(boolean vsync);
 
+    /**
+     * Also see {@link #setVSync(boolean)}
+     * @return {@code true} if VSync is enabled, {@code false} if VSync is disabled
+     */
     boolean isVSync();
 
+    /**
+     * Set a new {@link Canvas}
+     * @param canvas The new canvas
+     */
     void setCanvas(Canvas canvas);
 
+    /**
+     * @return Current canvas the renderer uses
+     */
     Canvas getCanvas();
 
+    /**
+     * @return Current maximum FPS the renderer is running on
+     */
     int getMaxFPS();
 
+    /**
+     * Sets the maximum fps. This value must be higher than 0
+     * @param fps The new maximum fps
+     */
     void setMaxFPS(int fps);
 
+    /**
+     * @return The current FPS
+     */
     int getFPS();
 
+    /**
+     * This method renders the current Canvas
+     */
     void render();
 }
