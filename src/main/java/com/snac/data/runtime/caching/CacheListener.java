9497bfe6795656e51b4afbab8408d6eaa3f24c7e
AC1original <ac1original.lol@gmail.com>
2025-05-27 23:04:24 +0200

    :bulb: Add documentation to CacheListener.java

--- src/main/java/com/snac/data/runtime/caching/CacheListener.java
+++ src/main/java/com/snac/data/runtime/caching/CacheListener.java
@@ -1,8 +1,56 @@
 package com.snac.data.runtime.caching;
 
+/**
+ * Listener interface for receiving events from a {@link Cache}.
+ * <p>
+ * Listeners must be registered with a specific {@code Cache} instance using
+ * {@link Cache#register(CacheListener)} in order to receive notifications.
+ * All listener methods are called <strong>before</strong> the corresponding action is performed on the cache.
+ *
+ * <p>Example usage:
+ * <pre>{@code
+ * Cache<String> cache = new Cache.CacheBuilder<String>().build();
+ * cache.register(new CacheListener() {
+ *     @Override
+ *     public void onCachedObjectExpire(Cache.CachedObject<?> object) {
+ *         // Called before the object expires
+ *     }
+ *
+ *     @Override
+ *     public void onCachedObjectRemove(Cache.CachedObject<?> object) {
+ *         // Called before the object is removed
+ *     }
+ *
+ *     @Override
+ *     public void onCachedObjectAdd(Cache.CachedObject<?> object) {
+ *         // Called before the object is added to the cache
+ *     }
+ * });
+ * }</pre>
+ *
+ * <p>You can unregister a listener using {@link Cache#unregister(CacheListener)}.
+ */
 public interface CacheListener {
 
+    /**
+     * Called before a cached object expires.
+     *
+     * @param object the object that is about to expire
+     */
     void onCachedObjectExpire(Cache.CachedObject<?> object);
+
+    /**
+     * Called before a cached object is removed from the cache.
+     *
+     * @param object the object that is about to be removed
+     */
     void onCachedObjectRemove(Cache.CachedObject<?> object);
+
+    /**
+     * Called before a new object is added to the cache.
+     *
+     * @param object the object that is about to be added
+     */
     void onCachedObjectAdd(Cache.CachedObject<?> object);
 }
+
