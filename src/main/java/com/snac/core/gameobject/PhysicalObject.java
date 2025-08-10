82eb49c0e6c3008dbd6dbfda8e1b12b11b5bbc3b
AC1original <ac1original.lol@gmail.com>
2025-08-10 21:48:30 +0200

    :construction: Add first version of PhysicalObject.java

--- src/main/java/com/snac/core/gameobject/PhysicalObject.java
+++ src/main/java/com/snac/core/gameobject/PhysicalObject.java
@@ -1,4 +1,38 @@
 package com.snac.core.gameobject;
 
-public class PhysicalObject extends AbstractObjectBase {
+import com.snac.util.Vector2D;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public abstract class PhysicalObject<I> extends AbstractObjectBase<I> {
+    protected final Vector2D velocity;
+
+    protected PhysicalObject() {
+        this(null, null, 0, 0);
+    }
+
+    protected PhysicalObject(@Nullable Vector2D position, @Nullable Vector2D direction, int width, int height) {
+        super(position, direction, width, height);
+
+        this.velocity = new Vector2D(0, 0);
+    }
+
+    public void onCollide(List<AbstractObjectBase<?>> collidedObjects) {}
+
+    @Override
+    void internalUpdate(double deltaTime) {
+        super.internalUpdate(deltaTime);
+
+        checkCollisions();
+    }
+
+    protected void checkCollisions() {
+        var manager = getManager();
+        if (manager == null || !manager.collides(this)) {
+            return;
+        }
+
+        onCollide(manager.getCollisions(this));
+    }
 }
