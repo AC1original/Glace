51812b2e501d16cdd8ec3ff89531764b71684408
AC1original <ac1original.lol@gmail.com>
2025-09-09 00:06:55 +0200

    :memo: Add documentation for AnimationFrame

--- src/main/java/com/snac/graphics/animation/AnimationFrame.java
+++ src/main/java/com/snac/graphics/animation/AnimationFrame.java
@@ -5,31 +5,31 @@ import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * This class represents a single frame of an animation.
+ * @param <I> The type of image this frame contains.
+ *          This type must be compatible with the used {@link com.snac.graphics.Renderer Renderer}
+ *          (which means it also must match {@link AnimationHandler} and {@link Animation}).
+ *           <br> For example, if you're using {@link com.snac.graphics.impl.SwingRenderer SwingRenderer},
+ *           this type must be {@link java.awt.image.BufferedImage BufferedImage}.
+ */
 @Getter
 @Setter
 public final class AnimationFrame<I> {
-    private static final AnimationFrame<?>[] empty;
     private I image;
     private int width;
     private int height;
-    private int delay = -1;
     @Nullable private Vector2D location;
 
-    private AnimationFrame(I image, int width, int height) {
+    /**
+     * Create a new AnimationFrame.
+     * @param image the image to use for this frame
+     * @param width the width of the image
+     * @param height the height of the image
+     */
+    public AnimationFrame(I image, int width, int height) {
         this.image = image;
         this.width = width;
         this.height = height;
-    }
-
-    public static AnimationFrame<?>[] createEmpty() {
-        return empty;
-    }
-
-    public static<I> AnimationFrame<I> create(I image, int width, int height) {
-        return new AnimationFrame<>(image, width, height);
-    }
-
-    static {
-        empty = new AnimationFrame[1];
     }
 }
